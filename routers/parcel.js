a14f5de8afb766430e3a3bd88e696fc183a33046
yjosh1 <84948881+yjosh1@users.noreply.github.com>
2022-09-01 15:42:03 +1000

    added functions

--- routers/parcel.js
+++ routers/parcel.js
@@ -4,5 +4,23 @@ const Sender = require('../models/sender');
 const Parcel = require('../models/parcel');
 
 module.exports = {
+    getAll: function (req, res) {
+        Parcel.find(function (err, parcels) {
+            if (err) {
+                return res.json(err);
+            } else {
+                res.json(parcels);
+            }
+        });
+    },
+
+    newSender: function (req, res) {
+        let newSenderDetails = req.body;
     
+        let sender = new Sender(newSenderDetails);
+        sender.save(function (err) {
+            console.log('Done');
+            res.json(sender);
+        });
+    },
 };
